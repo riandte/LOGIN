b9a98746957125c27c4689090323a4d504848293
Rian <rianduarte200@gmail.com>
2025-08-14 16:09:20 -0300

    Projeto final sem imagens antes e deois

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import jesley from '@/images/profile.jpg';
 export default function About() {
 	return (
 		<section className="py-15 px-10 bg-gray-950 text-center">
-			<h2 className="text-gray-100 text-3xl font-bold mb-6">Quem sou eu?</h2>
+			<h2 className="text-gray-100 text-4xl font-bold mb-6">Quem sou eu?</h2>
 
 			<div className="max-w-6xl mx-auto flex flex-col md:flex-row md:items-center">
 				<Image 
@@ -15,7 +15,7 @@ export default function About() {
 				height={400} 
 				className="rounded-full" />
 
-				<p className="text-gray-200 leading-relaxed md:pl-10 mt-6 md:mt-0 text-xl md:text-3xl text-center">
+				<p className="text-gray-200 leading-relaxed md:pl-10 mt-6 md:mt-0 text-xl md:text-2xl text-center">
 					Sou Jesley Slayter, Personal Trainer especializado em emagrecimento e qualidade de vida, atendendo iniciantes
 					e intermediários. Minha missão é transformar vidas através de treinos personalizados e acompanhamento próximo.
 				</p>
